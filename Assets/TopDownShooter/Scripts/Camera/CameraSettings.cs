100eadec9a9534eec88db66c858d71d46e0e0b15
CaptainEnginar <cihatcitakc@gmail.com>
2021-02-24 20:12:57 +0300

    +added: basic movement for camera

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float _rotationlerpSpeed = 1f;
+        public float RotationLerpSpeed { get { return _rotationlerpSpeed; } }
+
+
+        [Header("Position")]
+        [SerializeField] private Vector3 _positionOffset;
+        public Vector3 PositionOffset { get { return _positionOffset; } }
+
+        [SerializeField] private float _positionLerp = 1f;
+        public float PositionLerp { get { return _positionLerp; } }
+    }
+}
