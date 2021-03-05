0e2ba2473206046103783becc7e7c94d6b4323d8
CaptainEnginar <cihatcitakc@gmail.com>
2021-03-06 00:04:49 +0300

    +refactor player movement

---
+++ Assets/TopDownShooter/Scripts/PlayerControl/Movement/PlayerMovementSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerControls
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Player/Player Movement Settings")]
+    public class PlayerMovementSettings : ScriptableObject
+    {
+        public float HorizantalSpeed = 0.25f;
+        public float VerticalSpeed = 0.25f;
+
+    }
+}
